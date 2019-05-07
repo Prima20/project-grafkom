ee92873e4a4f7499e0b7027dc8b9c7f1e40dc759
Yasir Haq <yasirrhaq@users.noreply.github.com>
2019-05-07 11:31:13 +0700

    Init Commit

--- Grafika Komputer dan Visualisasi/vs/Lesson04/Demo.cpp
+++ Grafika Komputer dan Visualisasi/vs/Lesson04/Demo.cpp
@@ -643,7 +643,7 @@ void Demo::BuildSlider(int size, float xpos, float ypos) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	int width, height;
-	unsigned char* image = SOIL_load_image("kuning.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	unsigned char* image = SOIL_load_image("orange plastik.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -716,8 +716,6 @@ void Demo::BuildSlider(int size, float xpos, float ypos) {
 		16, 18, 17, 16, 19, 18,  // upper
 		20, 22, 21, 20, 23, 22,   // bottom
 		26,4,26,4,25,5
-
-
 	};
 
 	glGenVertexArrays(1, &VAOSlider);
